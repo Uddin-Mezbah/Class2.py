1248ffc057adaadedd78b8077421f361c515d6bd
Mezbah uddin(davide) <75944104+davide666777@users.noreply.github.com>
2021-02-23 23:17:43 +0800

    Add files via upload

---
+++ code13.py
@@ -0,0 +1,24 @@
+'''class Student:
+    pass
+
+harry = Student()
+davide = Student()
+
+harry.name = "Harry"
+harry.std = 12
+harry.section = 1
+davide.std = 10
+davide.subject = ["computer","electronics"]
+
+print(harry.section,davide.subject)'''
+
+class davide:
+    pass
+
+davide = ["age:25","section:A","subject: computer"]
+
+#davide.age = 10
+#davide.std = 5
+#davide.section = 5
+
+print(davide )
